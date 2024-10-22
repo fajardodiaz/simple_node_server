45a9bb6f99dcd0287df43765b3e8f122c4c2eeee
Pablo Fajardo <pfajardodiaz17@gmail.com>
2024-10-22 10:23:34 -0500

    Posts service created

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const { randomBytes } = require("crypto");
+const cors = require("cors");
+const axios = require("axios");
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use(cors());
+
+const posts = [];
+
+app.get("/posts", (req, res) => {
+  res.send(posts);
+});
+
+app.post("/posts", async (req, res) => {
+  const id = randomBytes(4).toString("hex");
+  const { title } = req.body;
+
+  const newPost = {
+    id: id,
+    title: title,
+  };
+
+  posts.push(newPost);
+
+  await axios.post("http://localhost:4005/events", {
+    type: "PostCreated",
+    data: newPost,
+  });
+
+  res.status(201).json({ message: "Post created successfully", newPost });
+});
+
+app.listen(PORT, () => {
+  console.log(`Posts app runnning on port ${PORT}`);
+});
