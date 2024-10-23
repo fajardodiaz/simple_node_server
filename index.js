827135e65ba1aaaaa7fd2152ca3f54ecaf0c596e
Pablo Fajardo <pfajardodiaz17@gmail.com>
2024-10-23 10:41:40 -0500

    Adding /posts endpoint

--- index.js
+++ index.js
@@ -19,24 +19,24 @@ app.get("/posts", (req, res) => {
   res.send(posts);
 });
 
-// app.post("/posts", async (req, res) => {
-//   const id = randomBytes(4).toString("hex");
-//   const { title } = req.body;
+app.post("/posts", async (req, res) => {
+  const id = randomBytes(4).toString("hex");
+  const { title } = req.body;
 
-//   const newPost = {
-//     id: id,
-//     title: title,
-//   };
+  const newPost = {
+    id: id,
+    title: title,
+  };
 
-//   posts.push(newPost);
+  posts.push(newPost);
 
-//   await axios.post("http://localhost:3005/events", {
-//     type: "PostCreated",
-//     data: newPost,
-//   });
+  // await axios.post("http://localhost:3005/events", {
+  //   type: "PostCreated",
+  //   data: newPost,
+  // });
 
-//   res.status(201).json({ message: "Post created successfully", newPost });
-// });
+  res.status(201).json({ message: "Post created successfully", newPost });
+});
 
 // app.post("/events", (req, res) => {
 //   console.log("Received event", req.body.type);
