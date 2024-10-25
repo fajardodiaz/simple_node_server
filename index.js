cc0d324dca62d14409a320c95e0911cbd455265d
Pablo Fajardo <41021378+fajardodiaz@users.noreply.github.com>
2024-10-25 11:40:34 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const express = require("express");
-// const { randomBytes } = require("crypto");
 const cors = require("cors");
-// const axios = require("axios");
 
 const app = express();
 const PORT = 3000;
@@ -9,10 +7,23 @@ const PORT = 3000;
 app.use(express.json());
 app.use(cors());
 
-const posts = [];
+const posts = [
+  {
+    id: "123abc",
+    title: "A simple post",
+  },
+  {
+    id: "678lki",
+    title: "Another post",
+  },
+];
 
 app.get("/success", (req, res) => {
   res.status(200).send({ message: "Success" });
+});
+
+app.get("/error", (req, res) => {
+  res.status(400).send({ error: "Bad Request, try again" });
 });
 
 app.get("/posts", (req, res) => {
@@ -30,19 +41,8 @@ app.post("/posts", async (req, res) => {
 
   posts.push(newPost);
 
-  // await axios.post("http://localhost:3005/events", {
-  //   type: "PostCreated",
-  //   data: newPost,
-  // });
-
   res.status(201).json({ message: "Post created successfully", newPost });
 });
-
-// app.post("/events", (req, res) => {
-//   console.log("Received event", req.body.type);
-
-//   res.send({});
-// });
 
 app.listen(PORT, () => {
   console.log(`Posts app runnning on port ${PORT}`);
